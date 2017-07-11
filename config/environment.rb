323ef1275db2c6a9f81747de9461b52291cc9614
hwchan91 <hwchan91@gmail.com>
2017-07-11 17:50:31 +0800

    configure to use sendgrid

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  :user_name => ENV["sendgrid_user"],
+  :password => ENV["sendgrid_password"],
+  :domain => 'https://damp-badlands-91919.herokuapp.com/'
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
